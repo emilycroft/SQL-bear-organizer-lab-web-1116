09675749a408fc8a7f518d02e7963baafdac5026
emilycroft <emilyhcroft@gmail.com>
2016-12-07 17:04:57 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO bears (name , age, gender, color, temperament , alive)
+VALUES("Mr. Chocolate", 2, "M", "brown", "mean", 1);
+
+INSERT INTO bears (name , age, gender, color, temperament , alive)
+VALUES("Rowdy", 5, "F", "green", "easygoing", 1);
+
+INSERT INTO bears (name , age, gender, color, temperament , alive)
+VALUES("Tabitha", 4, "F", "blue", "rowdy", 1);
+
+INSERT INTO bears (name , age, gender, color, temperament , alive)
+VALUES("Sargeant Brown", 5, "M", "light brown", "nice", 1);
+
+INSERT INTO bears (name , age, gender, color, temperament , alive)
+VALUES("Melissa", 3, "M", "yellow", "fluffy", 1);
+
+INSERT INTO bears (name , age, gender, color, temperament , alive)
+VALUES("Grinch", 6, "M", "red", "grinchy", 1);
+
+INSERT INTO bears (name , age, gender, color, temperament , alive)
+VALUES("Wendy", 4, "F", "black", "pleasant", 1);
+
+INSERT INTO bears (name , age, gender, color, temperament , alive)
+VALUES(NULL, 3, "M", "brown", "very angry", 1);
